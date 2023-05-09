eddd67c3b449174595663eb2d00fb7df408b225c
rohitprmsnls <rohit@prmsnls.xyz>
2023-05-09 11:31:19 +0530

    deploy

--- src/components/CartContainer.jsx
+++ src/components/CartContainer.jsx
@@ -59,8 +59,9 @@ const CartContainer = () => {
       return accumulator + item.quantity * item.price;
     }, 0);
     setTot(totalPrice);
-    
-  }, [cartItems]);
+    console.log(totalPrice)
+  }, [cartItems, tot]);
+  // console.log(tot)
   
   console.log(cartItems);
 
@@ -145,40 +146,42 @@ const CartContainer = () => {
               ))}
           </div>
           {/* cartTotal */}
-          <div className="w-full flex-1 bg-cartTotal rounded-t-[2rem] flex flex-col items-center justify-evenly px-8 py-2">
-            <div className="w-full flex items-center justify-between">
-              <p className="text-gray-400 text-lg">Sub Total</p>
-              <p className="text-gray-400 text-lg">${tot} </p>
-            </div>
-            <div className="w-full flex items-center justify-between">
-              <p className="text-gray-400 text-lg">Delivery</p>
-              <p className="text-gray-400 text-lg">$ 2.5 </p>
-            </div>
-            <div className="w-full border-b border-gray-600 my-2"></div>
-            <div className="w-full flex items-center justify-between">
-              <p className="text-gray-200 text-xl font-semibold">Total</p>
-              <p className="text-gray-200 text-xl font-semibold">
-                ${tot + 2.5}
-              </p>
-            </div>
-            {user ? (
-              <motion.button
-                whileTap={{ scale: 0.9 }}
-                type="button"
-                className="w-full p-2 rounded-full bg-gradient-to-r from-orange-400 to-orange-600 text-gray-50 text-lg my-2 hover:shadow-lg  "
-              >
-                Check Out
-              </motion.button>
-            ) : (
-              <motion.button
-                whileTap={{ scale: 0.9 }}
-                type="button"
-                className="w-full p-2 rounded-full bg-gradient-to-r from-orange-400 to-orange-600 text-gray-50 text-lg my-2 hover:shadow-lg  "
-              >
-                Log In
-              </motion.button>
-            )}
-          </div>
+          
+              <div className="w-full flex-1 bg-cartTotal rounded-t-[2rem] flex flex-col items-center justify-evenly px-8 py-2">
+                <div className="w-full flex items-center justify-between">
+                  <p className="text-gray-400 text-lg">Sub Total</p>
+                  <p className="text-gray-400 text-lg">${tot} </p>
+                </div>
+                <div className="w-full flex items-center justify-between">
+                  <p className="text-gray-400 text-lg">Delivery</p>
+                  <p className="text-gray-400 text-lg">$ 2.5 </p>
+                </div>
+                <div className="w-full border-b border-gray-600 my-2"></div>
+                <div className="w-full flex items-center justify-between">
+                  <p className="text-gray-200 text-xl font-semibold">Total</p>
+                  <p className="text-gray-200 text-xl font-semibold">
+                    ${tot + 2.5}
+                  </p>
+                </div>
+                {user ? (
+                  <motion.button
+                    whileTap={{ scale: 0.9 }}
+                    type="button"
+                    className="w-full p-2 rounded-full bg-gradient-to-r from-orange-400 to-orange-600 text-gray-50 text-lg my-2 hover:shadow-lg  "
+                  >
+                    Check Out
+                  </motion.button>
+                ) : (
+                  <motion.button
+                    whileTap={{ scale: 0.9 }}
+                    type="button"
+                    className="w-full p-2 rounded-full bg-gradient-to-r from-orange-400 to-orange-600 text-gray-50 text-lg my-2 hover:shadow-lg  "
+                  >
+                    Log In
+                  </motion.button>
+                )}
+              </div>
+         
         </motion.div>
       ) : (
         <div className="w-full h-full flex flex-col items-center justify-center  gap-6">
